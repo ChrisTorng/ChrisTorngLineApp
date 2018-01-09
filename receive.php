263ca18af80c180a7f4e86ba87c24ae8b17b373b
ChrisTorng <ChrisTorng@hotmail.com>
2018-01-09 14:16:58 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -33,7 +33,7 @@
 
  //回傳給line server
  $header[] = "Content-Type: application/json";
- $header[] = "Authorization: Bearer n4mZIQp9UqWXhCEgIg1fLmyjUeDMgCe/bF+4EOBDZ7fGscOgNGFsHTr3fGco/E7A5hq7A7jiDszSCk/j3pVVPbx7nf0E+FKe5jX6syQGOxO7kwp5lmZ3zRES1qxceq/N+/E9Qy5gSDbBx56l8sScTwdB04t89/1O/w1cDnyilFU=";
+ $header[] = "Authorization: Bearer FjeOS2XLpz35RcwPQ8TVgtUF8znXP8CKYqQmJLkhNM3xdwVaUg2kfkRqKIifkGm0HMaO1Eq/LjuZxzcDy1sLutRhi/y0EPVjO/UYpBEiD8VLlMWHyvAUMDNJ0Q0tF0XCRtubA5l1kQJ9v6UFnNDr6AdB04t89/1O/w1cDnyilFU=";
  $ch = curl_init("https://api.line.me/v2/bot/message/push");                                                                      
  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  

515825cb9046ce6885a33f945656cb02f720d7fa
ChrisTorng <ChrisTorng@hotmail.com>
2018-01-09 13:52:47 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -2,7 +2,11 @@
  $json_str = file_get_contents('php://input'); //接收REQUEST的BODY
  $json_obj = json_decode($json_str); //轉JSON格式
 
- //產生回傳給line server的格式
+ $myfile = fopen("log.txt","w+") or die("Unable to open file!"); //設定一個log.txt 用來印訊息
+ fwrite($myfile, "\xEF\xBB\xBF".json_decode($json_obj)); //在字串前加入\xEF\xBB\xBF轉成utf8格式
+ fclose($myfile);
+
+//產生回傳給line server的格式
 	$sender_userid = $json_obj->events[0]->source->userId;
 	$sender_txt = $json_obj->events[0]->message->text;
  $response = array (
@@ -14,10 +18,6 @@
 					)
 				)
 		);
-
- $myfile = fopen("log.txt","w+") or die("Unable to open file!"); //設定一個log.txt 用來印訊息
- fwrite($myfile, "\xEF\xBB\xBF".json_decode($json_obj)); //在字串前加入\xEF\xBB\xBF轉成utf8格式
- fclose($myfile);
 
  //回傳給line server
 	$header[] = "Content-Type: application/json";

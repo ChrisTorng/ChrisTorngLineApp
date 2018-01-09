ba3f075c778a2a143944b383b213b1ba915cedee
ChrisTorng <ChrisTorng@hotmail.com>
2018-01-09 13:55:58 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -1,10 +1,13 @@
 <?php
+ echo("1");
  $json_str = file_get_contents('php://input'); //接收REQUEST的BODY
  $json_obj = json_decode($json_str); //轉JSON格式
+ echo("2");
 
  $myfile = fopen("log.txt","w+") or die("Unable to open file!"); //設定一個log.txt 用來印訊息
  fwrite($myfile, "\xEF\xBB\xBF".json_decode($json_obj)); //在字串前加入\xEF\xBB\xBF轉成utf8格式
  fclose($myfile);
+ echo("3");
 
 //產生回傳給line server的格式
 	$sender_userid = $json_obj->events[0]->source->userId;
@@ -18,6 +21,7 @@
 					)
 				)
 		);
+ echo("4");
 
  //回傳給line server
 	$header[] = "Content-Type: application/json";
@@ -29,5 +33,6 @@
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $header);                                                                                                   
 	$result = curl_exec($ch);
 	curl_close($ch); 
+ echo("5");
 
 ?>

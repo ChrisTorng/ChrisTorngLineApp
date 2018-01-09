1193aec0c0f6f5b31eee02e5232c9e16616a5946
ChrisTorng <ChrisTorng@hotmail.com>
2018-01-09 11:36:33 +0800

    Get line message and write to log.txt

--- receive.php
+++ receive.php
@@ -1,3 +1,8 @@
 <?php
-echo "Hello! I'm ChrisTorng"
+  $json_str = file_get_contents('php://input'); // receive request's body
+  $json_obj = json_decode($json_str); // decode json string into json object
+
+  $myfile = fopen("log.txt", "w+") or die("Unable to open file!");
+  fwrite($myfile, "\xEF\xBB\xBF".$json_str); // Turn into UTF-8 format
+  fclose($myfile);
 ?>
